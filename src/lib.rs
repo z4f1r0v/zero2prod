b2b8d35b59c884a9809939c63bed4e9f9ebc0d7e
Alexander Zafirov <alzaf@leo-pharma.com>
2021-12-18 22:19:08 +0100

    Test with random port

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 
@@ -5,10 +6,11 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new().route("/health_check", web::get().to(health_check))
+    })
+        .listen(listener)?
         .run();
-    // No .await here!
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,20 +1,25 @@
-#[actix_rt:: test]
-async fn health_check_works() {
-    spawn_app();
+use std::net::TcpListener;
 
+#[actix_rt::test]
+async fn health_check_works() {
+    let address = spawn_app();
     let client = reqwest::Client::new();
-    // Act
+
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
-    // Assert
+
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
