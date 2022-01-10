d5e8a55a167bce87905472c5e9b78576a14b7d1e
Alexander Zafirov <alzaf@leo-pharma.com>
2022-01-10 22:29:55 +0100

    Containerization

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,9 +1,16 @@
 use secrecy::{ExposeSecret, Secret};
+use std::convert::{TryFrom, TryInto};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -17,22 +24,69 @@ pub struct DatabaseSettings {
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
-    settings.merge(config::File::with_name("configuration"))?;
+    let base_path = std::env::current_dir().expect("Failed to dermine the current directory.");
+    let configuration_directory = base_path.join("configuration");
+
+    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    settings.merge(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true),
+    )?;
+
     settings.try_into()
 }
 
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environement. Use either `local` or `production`",
+                other
+            ))
+        }
+    }
+}
 impl DatabaseSettings {
     pub fn connection_string(&self) -> String {
         format!(
             "postgres://{}:{}?dbname={}&user={}&password={}",
-            self.host, self.port, self.database_name, self.username, self.password.expose_secret()
+            self.host,
+            self.port,
+            self.database_name,
+            self.username,
+            self.password.expose_secret()
         )
     }
     pub fn connection_string_without_db(&self) -> String {
         format!(
             "postgres://{}:{}?user={}&password={}",
-            self.host, self.port, self.username, self.password.expose_secret()
+            self.host,
+            self.port,
+            self.username,
+            self.password.expose_secret()
         )
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use sqlx::postgres::PgPool;
+use sqlx::postgres::PgPoolOptions;
 
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -12,10 +12,14 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration:: from_secs(2))
+        .connect_lazy(&configuration.database.connection_string())
+        .expect("Failed to get connection pool.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
